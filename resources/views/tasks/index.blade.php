93b34c5949afeca14fb8dead49ed2443c39ec587
s.kurahashi <work.wb14@gmail.com>
2020-02-02 15:19:32 +0000

    user registration and user login

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,27 +2,20 @@
 
 @section('content')
 
-    <h1>タスク一覧</h1>
+    @if(Auth::check())
+        <h1>{{ Auth::user()->name }} のタスク一覧</h1>
 
-    @if (count($tasks) > 0 )
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>ステータス</th>
-                    <th>タスク</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                    <td>{{ $task->status }}</td>
-                    <td>{{ $task->content }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        @if (count($tasks) > 0 )
+            @include('tasks.tasks')
+        @endif
+        {!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'btn btn-primary']) !!}
+        
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Tasklist へようこそ！</h1>
+                {!! link_to_route('signup.get', '新規登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
     @endif
-    {!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'btn btn-primary']) !!}
 @endsection
